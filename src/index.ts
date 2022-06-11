f8294c9d98416fafcca16b454a0fa79a2d3319b7
Raimondas Rimkevičius <github@mekdrop.name>
2022-06-11 23:40:09 +0300

    Some prettier fixes

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,39 @@
 import {error, getInput, info, setFailed} from '@actions/core'
 import actions from './data/actions'
 import generators from './data/generators'
-import UnknownSelectedGeneratorNameError from "./errors/UnknownSelectedGeneratorNameError";
+import UnknownSelectedGeneratorNameError from './errors/UnknownSelectedGeneratorNameError'
 import NotAllRequiredArgumentsDefinedForSelectedEngineError
-  from "./errors/NotAllRequiredArgumentsDefinedForSelectedEngineError";
+  from './errors/NotAllRequiredArgumentsDefinedForSelectedEngineError'
 
 try {
   const generatorName = getInput('engine', {required: false})
   if (typeof generators[generatorName] == 'undefined') {
-    throw new UnknownSelectedGeneratorNameError();
+    throw new UnknownSelectedGeneratorNameError()
   }
-  const generator = generators[generatorName];
+  const generator = generators[generatorName]
   if (!generator.checkIfAllInputOptionsDefined()) {
-    throw new NotAllRequiredArgumentsDefinedForSelectedEngineError();
+    throw new NotAllRequiredArgumentsDefinedForSelectedEngineError()
   }
 
   for (const action of actions) {
     if (!action.shouldRun(generator)) {
-      continue;
+      continue
     }
-    const desc = action.getDescription();
+    const desc = action.getDescription()
     if (desc !== null) {
-      info(desc);
+      info(desc)
     }
-    action.exec(generator);
+    action.exec(generator)
   }
 } catch (err) {
   if (err instanceof Error) {
     if (err.stack) {
       for (const line of err.stack.split('\n')) {
-        error(line);
+        error(line)
       }
     }
-    setFailed(err.message);
+    setFailed(err.message)
   } else {
-    setFailed(
-      String(err)
-    );
+    setFailed(String(err))
   }
 }
