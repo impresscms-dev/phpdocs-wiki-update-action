249cd2f765cf1cf2fb7445bd8e891f71713bf0b9
Raimondas Rimkevičius <github@mekdrop.name>
2022-07-05 19:00:44 +0300

    -f -> -d

--- bin/checkout.sh
+++ bin/checkout.sh
@@ -6,7 +6,7 @@ GITHUB_USER="$2"
 # shellcheck disable=SC2086
 git clone -q https://$GITHUB_USER:$PRIVATE_TOKEN@github.com/$GITHUB_REPOSITORY.wiki.git "$OLD_WIKI_CHECKOUT_PATH"
 
-if [ ! -f "$OLD_WIKI_CHECKOUT_PATH" ]; then
+if [ ! -d "$OLD_WIKI_CHECKOUT_PATH" ]; then
   echo "ERROR: wiki directory not created. Perhaps You didn't created at least one web page?"
   exit 1
 fi
