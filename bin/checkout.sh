1a15692ca0cd10aa6b166f05caecbaea3da076cd
Raimondas Rimkevičius <github@mekdrop.name>
2022-07-05 19:36:07 +0300

    quotes for remote

--- bin/checkout.sh
+++ bin/checkout.sh
@@ -4,10 +4,10 @@
 pushd "$OLD_WIKI_CHECKOUT_PATH"
 
   if git show-ref -q --heads "$GITHUB_REF_NAME"; then
-    echo "Remote $GITHUB_REF_NAME found."
+    echo "Remote '$GITHUB_REF_NAME' found."
     git checkout "$GITHUB_REF_NAME"
   else
-    echo "Remote $GITHUB_REF_NAME not found. Creating."
+    echo "Remote '$GITHUB_REF_NAME' not found. Creating."
     git checkout -b "$GITHUB_REF_NAME"
     git push --set-upstream origin "$GITHUB_REF_NAME"
   fi;
