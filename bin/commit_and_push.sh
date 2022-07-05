9b6fde300e5535d7dd9c008d2f1706579f5d18d8
Raimondas Rimkevičius <github@mekdrop.name>
2022-07-05 06:12:40 +0300

    Modified a way how merging when conflicts happens works
    I hope this will fix a bug when in such cases action didn't produce the desired result.

--- bin/commit_and_push.sh
+++ bin/commit_and_push.sh
@@ -7,25 +7,28 @@ COMMITER_EMAIL="$3"
 # shellcheck disable=SC2164
 pushd "$NEW_WIKI_CHECKOUT_PATH"
 
+  echo "Configuring GIT..."
   git config user.email "$COMMITER_EMAIL" || exit 1
   git config user.name "$COMMITER_NAME" || exit 2
   git config advice.addEmptyPathspec false
-  git config pull.rebase true
+  git config pull.rebase false
   git config advice.skippedCherryPicks false
   git config advice.setUpstreamFailure false
 
-  git pull
-
   # idea from https://stackoverflow.com/a/10135446/1762839
+  echo "Adding changes to GIT..."
   git ls-files --modified | grep '\.md$' | xargs git add
   git ls-files . --exclude-standard --others | grep '\.md$' | xargs git add
   git ls-files --deleted | grep '\.md$' | xargs git add
 
+  echo "Commiting..."
   git commit -m "$COMMIT_MESSAGE"
 
+  echo "Pulling and merging from remote..."
   git branch --set-upstream-to=origin/$GITHUB_REF_NAME "$GITHUB_REF_NAME" || true
   git pull -s recursive -X ours || true
 
+  echo "Pushing to remote..."
   git push --set-upstream origin "$GITHUB_REF_NAME" || exit 3
 
 # shellcheck disable=SC2164
