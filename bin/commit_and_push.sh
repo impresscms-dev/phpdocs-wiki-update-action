091b41f420ac59e422bb86338cce4f79063e4321
Raimondas Rimkevičius <github@mekdrop.name>
2022-07-05 19:39:41 +0300

    git push more beautifull

--- bin/commit_and_push.sh
+++ bin/commit_and_push.sh
@@ -7,25 +7,17 @@ COMMITER_EMAIL="$3"
 # shellcheck disable=SC2164
 pushd "$NEW_WIKI_CHECKOUT_PATH"
 
-  echo "Configuring GIT..."
-  git config user.email "$COMMITER_EMAIL" || exit 1
-  git config user.name "$COMMITER_NAME" || exit 2
-  git config advice.addEmptyPathspec false
-  git config pull.rebase false
-  git config advice.skippedCherryPicks false
-  git config advice.setUpstreamFailure false
-
   echo "Adding changes to GIT..."
   git add -A --verbose
 
-  echo "Commiting..."
+  echo "Committing..."
   git commit -m "$COMMIT_MESSAGE"
 
   echo "Pulling and merging from remote..."
   git pull -s recursive -X ours || true
 
   echo "Pushing to remote..."
-  git push --set-upstream origin "$GITHUB_REF_NAME" || exit 3
+  git push || exit 1
 
 # shellcheck disable=SC2164
 popd
